d08f84775a36a859aa1f2ac8013fe4c8654f1ceb
Michael Lorang <mlorang@udel.edu>
2023-06-20 17:03:17 -0400

    added options

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,27 +1,23 @@
 import React from 'react'
-import {Form} from "react-bootstrap";
+import '../style.css';
 
 interface RenterName {
   name: string;
-  setName: (newString: string) => void;
 }
 
-function Name({name, setName}: RenterName): JSX.Element {
-  function updateName(event: React.ChangeEvent<HTMLInputElement>) {
-    setName(event.target.value);
-  }
+function Name({name}: RenterName): JSX.Element {
+
 
 
   return (
     <div>
-    <Form.Group controlId="formAnswerbox">
-      <Form.Control value={name} onChange={updateName} />
-    </Form.Group><div className="body">Hi {name}!
+    <div className="body">
+      Hi {name}!
       Thank you for booking the XXXXXX XXXXXX XXXXXX. Please see and review attached guidelines for all
       information. Please be sure to print and return the forms that require signatures or initials (highlighted for
       your convenience) – Looks like the only signature needed is the COVID Waiver (final page). Also
       attached is a floor plan of the Main Hall if you wish to diagram your table/chair layout for your rental –
-        Table and chair dimensions are included on this page as well.  </div>
+      Table and chair dimensions are included on this page as well.  </div>
       </div>
   )
 }

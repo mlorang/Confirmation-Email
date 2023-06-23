cf674b93f097daa841f6e0dc45c191ff9cb78174
Michael Lorang <mlorang@udel.edu>
2023-06-23 14:58:45 -0400

    ChangeEventPrice component completed

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,23 @@ function App(): JSX.Element {
   const [endTime, setEndTime] = useState<string>("");
   const [rooms, setRooms] = useState<string>("");
   const [balanceDue, setBalanceDue] = useState<string>("");
-  const [hours, setHours] = useState<number>(0);
+  const [hours, setHours] = useState<string>("");
   const [price, setPrice] = useState<number>(0);
 
   return (
     <div>
       <div className="body">
         <ChangeRenterName name={name} setName={setName}></ChangeRenterName>
+
         <ChangeEventDate date={date} setDate={setDate}></ChangeEventDate>
+
         <ChangeEventTime
           startTime={startTime}
           setStartTime={setStartTime}
           endTime={endTime}
           setEndTime={setEndTime}
         ></ChangeEventTime>
+
         <ChangeEventPrice
           balanceDue={balanceDue}
           setBalanceDue={setBalanceDue}
@@ -38,15 +41,19 @@ function App(): JSX.Element {
           price={price}
           setPrice={setPrice}
         ></ChangeEventPrice>
+
         <ChangeEventRoom rooms={rooms} setRooms={setRooms}></ChangeEventRoom>
+
         <EventName name={name}></EventName>
+
         <EventDateTimeRoom
           date={date}
           startTime={startTime}
           endTime={endTime}
           rooms={rooms}
         ></EventDateTimeRoom>
-        <EventBalanceDue hours={hours} price={price}></EventBalanceDue>
+
+        <EventBalanceDue price={price}></EventBalanceDue>
       </div>
     </div>
   );
